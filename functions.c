07c49aa2444f7cfce08464910a1f0d159e7ce16a
Akshay Kumar <gakkikumar@gmail.com>
2018-03-13 00:24:34 +0530

    Update functions.c
    added header file

--- functions.c
+++ functions.c
@@ -1,10 +1,11 @@
 //use of functions in c examples
-
+#include<stdio.h>
 void fun(int a,float b);//function prototype that return void
 
 int main()//main function returing an integer
 {
-  int a=1; float b=1.0;
+  int a=1; 
+  float b=1.0;
   fun(a,b);//function call with formal parameters
   return 0;//return value;
 }
