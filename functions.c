3e3236fcf4966ec68f9ca83868565f01fde5d38e
Akshay Kumar <gakkikumar@gmail.com>
2018-03-13 00:22:41 +0530

    Create functions.c
    functions examples

---
+++ functions.c
@@ -0,0 +1,14 @@
+//use of functions in c examples
+
+void fun(int a,float b);//function prototype that return void
+
+int main()//main function returing an integer
+{
+  int a=1; float b=1.0;
+  fun(a,b);//function call with formal parameters
+  return 0;//return value;
+}
+void fun(int a,float b)
+{
+  printf("FUNCTIONS");
+}
